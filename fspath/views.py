710937d028afe51d3a03b90b97cf9214fcf42f8f
Ali <ali.ozgur@example.com>
2022-12-31 21:52:59 +0300

    created Student Model

--- fspath/views.py
+++ fspath/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse # respone import ettik.
 
 def selam(request):
-    return HttpResponse("<h1>Merhaba Django</h1>")
+    return HttpResponse("<h1>Merhaba Django</h1>") # anasayfada göstermek için bir respone fonksiyonu tanımladım.
